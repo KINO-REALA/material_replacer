c2199fdbadbf99782f186c7dead72555ef0a1e36
Kino <kino.reala@gmail.com>
2022-09-26 15:54:03 +0900

    update

--- material_replacer.py
+++ material_replacer.py
@@ -12,7 +12,6 @@ def validate_material(mat_name, all_materials):
     else:
         cmds.warning('Material not found: {}'.format(mat_name))
         return False
-    
 
 def get_material_name_from_object(obj):
     result = re.search('_\d{3}', obj)
@@ -20,17 +19,31 @@ def get_material_name_from_object(obj):
         num = result.group()
         mat = obj.split('_')[-2]
         mat_name = mat+num
-        print('mat_name', mat_name)
         return mat_name
     return None
 
-selected_list = cmds.ls(sl=True)    
-
+def get_current_material_name(obj):
+    cmds.select(obj)
+    # get shapes of selection:
+    shapes =  cmds.ls(dag=True,o=True,s=True,sl=True)
+    if len(shapes)>1:
+        print('{} is group object'.format(obj))
+        return None
+    # get shading groups from shapes:
+    shading_grps = cmds.listConnections(shapes,type='shadingEngine')
+    # get the shaders:
+    shaders = cmds.ls(cmds.listConnections(shading_grps), materials=True)
+    if len(shaders)>1:
+        cmds.warning('{} has more than one material.'.format(obj))
+    if len(shaders)==1:
+        return shaders[0]
+    if len(shaders)==0:
+        cmds.warning('{} has more than one shape.'.format(obj))
+        return None
 
 def assign_material(mat_name):
     if mat_name:
         cmds.hyperShade(a=mat_name)
-
 
 def list_all_children(nodes):    
     result = set()
@@ -43,14 +56,17 @@ def list_all_children(nodes):
 
 def main():
     all_materials = get_all_materials()
-
     parent = cmds.ls(sl=True)[0]
     objs = list_all_children(parent)
     for obj in objs:
         mat_name = get_material_name_from_object(obj)
+        current_mat = get_current_material_name(obj)
         if mat_name and validate_material(mat_name, all_materials):
-            assign_material(mat_name)
+            if mat_name != current_mat:
+                assign_material(mat_name)
+                print('Object: {}, Material:{} >>> {}'.format(obj, current_mat, mat_name))
     
 
 if __name__ =='__main__':
     main()
+
